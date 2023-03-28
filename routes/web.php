f31b9a10118826bafc5f42d3ef42c8c20e454e6a
zhaojingning <784955168@qq.com>
2023-03-28 13:09:39 +0800

    注册与登录

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', 'PagesController@root')->name('root');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
