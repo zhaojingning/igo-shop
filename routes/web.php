e772fca649292a52946ad4836c3d9461f36d01d8
zhaojingning <784955168@qq.com>
2023-03-28 15:57:45 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // 启用邮箱验证相关的路由（验证邮箱页面，重发验证邮件页面等）
 Auth::routes(['verify' => true]);
+
+// auth 中间件代表需要登录，verified中间件代表需要经过预想认证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+	Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
