bc3f1a4cc2a186099d78e426fac7e11dbffd8a32
zhaojingning <784955168@qq.com>
2023-03-29 11:45:49 +0800

    新增收货地址

--- config/app.php
+++ config/app.php
@@ -82,7 +82,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh_CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -178,7 +178,8 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+        // Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ Auth::routes(['verify' => true]);
 // auth 中间件代表需要登录，verified中间件代表需要经过预想认证
 Route::group(['middleware' => ['auth', 'verified']], function() {
 	Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+	Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+	Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
+
