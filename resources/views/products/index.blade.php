9457b20bbf4bc9fb88befdb10582f45a25327439
zhaojingning <784955168@qq.com>
2023-03-30 12:34:16 +0800

    用户界面 - 商品详情页

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -34,7 +34,11 @@
         <div class="col-3 product-item">
           <div class="product-content">
             <div class="top">
-              <div class="img"><img src="{{ $product->image }}" alt="" referrerpolicy="no-referrer"></div>
+              <div class="img">
+                <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                  <img src="{{ $product->image }}" alt="" referrerpolicy="no-referrer">
+              </a>
+              </div>
               <div class="price"><b>￥</b>{{ $product->price }}</div>
               <div class="title">{{ $product->title }}</div>
             </div>
