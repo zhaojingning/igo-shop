a2f0155b83932df42197e8f9b25c0109bd1a9c7a
zhaojingning <784955168@qq.com>
2023-04-01 17:47:49 +0800

    集成支付宝

--- config/pay.php
+++ config/pay.php
@@ -4,19 +4,25 @@ declare(strict_types=1);
 
 use Yansongda\Pay\Pay;
 
+$appPrivateKey = file_get_contents(config_path('alipay/appPrivateKey'));
+$str            =  chunk_split(base64_encode($appPrivateKey), 64, "\n");
+$appPrivateKey = "-----BEGIN RSA PRIVATE KEY-----\n$str-----END RSA PRIVATE KEY-----\n";
+
+$appPrivateKey = 'MIIEogIBAAKCAQEAoWvLG2FT9l/lXrQ411mo10oheFAnR9ze6DRJNSf60VR2SH66gbWYTyIryAdRsiCF2TWbva4c7pjeeukMLpVydBVPzmnMV5/oMJ4Gzk0NU/rfcRStQ/FvMVdADhRESYv8aQXWq7YUVYr4UAVGc7+ggL1e9keUinnV2g8ds6tUoMSwSumJVjKNIuRGRN8UpPg4wTiH/IGBp+Vvp381leo1eiazAa9lw+SrQ5Phx92vYu18kWwYjr3o6ZAMvhQdv/gM2hsd7Ge/t34y6gBFLzScMTy1mz4YmsBtzpaMcMI4DN8ZNwLxHgASH8CiOVq5sixeCDkaa/CVlqFn6cljSkOUqQIDAQABAoIBADcFqjbMBzWs3F9aqSFms3GnGB+NmsYZGHYoFGglF46w7pypbeN2xdL7f9bv/73q7hTq/Ao8xiniO7vDol5inJ/K/+gJEhkwLZIc248Inqloky9Vb/X/3vn/lpr194zmRYR69ACV0PqToy6Ljcc4o3oekrb6ETzeaLGqg2JR3UfrGNqxbpjXE2K4gVCQBKGteZcA2sjMj9916H/EE98j6whITPKZ/KUeRB6NZuRrijzG+CNFk7uJBOFli/67/l9ixcGfgJYBaRLWw514jWllZ8qU1bRe3LOMq0YOOkbamIZSenQNHpt5UDV2YKWNQQkPa3LIoz6fEi5DRANjPlyjZAECgYEAz7YA2y1hscAY0w52otfkms0Xrbyxxp3IRjyW5YN4J7slju1xopKtwJ5veMqbraB3RNkcY3hHBx0i1+p4Lo1tGS9umwkM5LCc66ICdVuF0QpX7AzeUhqnwBNNTuxPVDopuPGSM9m8b/rTQaJd7uLnrM9xYK8H5L2FDKw/Lt6rrSkCgYEAxvLUeTBwutHOjiRpjc0Bs8pdIibh6Nr+amjlOQkP96wafMGGe8Ay/p8er8RVCLZKBKzjpECGTVgDZMtEO0BrPbE8w7uxUZY+YkoKdAvNrO7Bq9mczzGR2zrlfZaLlNfdiKBj3MkkKDEdHhP6/H7iXI37jsuP+LJzqlEcN3hOG4ECgYA7eFTObDPofKOe6ik1frLJT6dT6w7LWymYUoixte6VEZQzU/CRJ4Xv/GzWHMxt7d+4KqFiKXHMq57qSwuV6JAwAdCdOv4iDB8hqAUkLMwnidqajySoRVF6QQyFaEteRjjj5uaBSfoV5a7Ov/o9B2JlrA8+K18LMugBNN/yP3bP4QKBgDeg7KJaVhsW0U/ThKQ+v4wbOIF1J9PpcBwm6nlRPGK/f8SEhGsT6e5iZb02A6Tk+7tk8F4cILF9u60yXQjIUXQ1m80LLWCnxxfKpjOenUsOk57OwVb6AFOxmzvLnYnn9ize1C2HHJIUcZTYd2SkwfypQr7B0qbylv73oREtlYGBAoGAfVluo9mHIuPz7rHI/eGQVS3wBQlKv/AXbK0SOuObwp3SE9pNbeYlBQKaYtbjsmw/LEoP58mE/Y6lcSURFyyXxZMUvK6X4Sx55xZyGos+zkmBjuwuJa0M0o+AtoIfZRxEFPKLryc0Rc+fmlX/Q3zluFGrVKaf+VlEKgvxib41nhE=';
+
 return [
     'alipay' => [
         'default' => [
             // 必填-支付宝分配的 app_id
-            'app_id' => '',
+            'app_id' => '2021000122671497',
             // 必填-应用私钥 字符串或路径
-            'app_secret_cert' => '',
+            'app_secret_cert' => $appPrivateKey,
             // 必填-应用公钥证书 路径
-            'app_public_cert_path' => '',
+            'app_public_cert_path' => config_path('alipay/appPublicCert.crt'),
             // 必填-支付宝公钥证书 路径
-            'alipay_public_cert_path' => '',
+            'alipay_public_cert_path' => config_path('alipay/alipayPublicCert.crt'),
             // 必填-支付宝根证书 路径
-            'alipay_root_cert_path' => '',
+            'alipay_root_cert_path' => config_path('alipay/alipayRootCert.crt'),
             'return_url' => '',
             'notify_url' => '',
             // 选填-服务商模式下的服务商 id，当 mode 为 Pay::MODE_SERVICE 时使用该参数
