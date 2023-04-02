f308e31abbc7e9ba01670840d9a61f224c3fec44
zhaojingning <784955168@qq.com>
2023-04-02 16:51:32 +0800

    管理后台 - 订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -7,8 +7,14 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 
+use Illuminate\Http\Request;
+use App\Exceptions\InvalidRequestException;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
 class OrdersController extends AdminController
 {
+    use ValidatesRequests;
     /**
      * Title for current resource.
      *
@@ -121,5 +127,35 @@ class OrdersController extends AdminController
         return $content->header('查看订单')
         ->body(view('admin.orders.show', ['order' => Order::find($id)]));
     }
-    
+
+    // 发货 接口
+    public function ship(Order $order, Request $request)
+    {
+        // 判断当前订单是否已经支付
+        if(!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+        // 判断该订单发货状态是否为未发货
+        if($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
+        // Laravel 5.5之后 validate 方法可以返回校验过的值
+        $data = $this->validate($request, [
+            'express_company'   => ['required'],
+            'express_no'    => ['required'],
+        ], [], [
+            'express_company'   => '物流公司',
+            'express_no'    => '物流单号',
+        ]);
+        // 将订单发货状态改为已发货，并存入物流信息
+        $order->update([
+            'ship_status'   => Order::SHIP_STATUS_DELIVERED,
+            // 我们在 Order 机型的 $casts属性里指明了 ship_data是一个数组
+            // 因此这里可能直接把数据传过去
+            'ship_data' => $data,
+        ]);
+        // 返回上一页
+        return redirect()->back();
+    }
+
 }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,4 +16,5 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
