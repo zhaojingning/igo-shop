025fd3d13afc61b802d05ddd424aeb8ee56d924a
zhaojingning <784955168@qq.com>
2023-04-02 17:31:49 +0800

    确认收货

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,8 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
 
+use App\Exceptions\InvalidRequestException;
+
 class OrdersController extends Controller
 {
     // 利用 Laravel 的自动解析功能注入 CartService 类
@@ -36,5 +38,20 @@ class OrdersController extends Controller
     {
         $this->authorize('own', $order);
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function received(Order $order, Request $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        // 判断订单的发货状态是否已经发货
+        if($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+        // 更新发货状态
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+        return $order;
+        // 因为前端确认收货的操作从表单提交改成了 AJAX 请求，因此控制器中的返回值需修改成上面 // 返回原页面
+        // return redirect()->back();
     }
 }
