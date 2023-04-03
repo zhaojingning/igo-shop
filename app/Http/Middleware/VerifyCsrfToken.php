6f1e22f38a1d1bdb1ae925d3a36b56b55a93d7bb
zhaojingning <784955168@qq.com>
2023-04-03 14:38:22 +0800

    管理后台 - 同意退款 - 微信支付退款

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'payment/alipay/notify',
-        'payment/wechat/notify'
+        'payment/wechat/notify',
+        'payment/wechat/refund_notify'
     ];
 }
